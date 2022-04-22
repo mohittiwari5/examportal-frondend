0653fa3ca61da4748e1e68276a00a81f14b1297a
mohittiwari5 <mohittiwarimt127@gmail.com>
2022-04-22 13:05:22 +0530

    Encoded password in backend and Login and register working properly

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { identifierName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
 
@@ -13,67 +14,70 @@ export class LoginComponent implements OnInit {
 
 
   loginData = {
-    username:'',
-    password:''
+    username: '',
+    password: ''
   }
 
-  constructor(private snack:MatSnackBar,private loginService:LoginService) { }
+  constructor(private snack: MatSnackBar, private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  public formSubmit(){
-    
-    if(this.loginData.username.trim()=="" || this.loginData.username==null){
-      this.snack.open("Username is required",'',{
-        duration:3000
+  public formSubmit() {
+
+    if (this.loginData.username.trim() == "" || this.loginData.username == null) {
+      this.snack.open("Username is required", '', {
+        duration: 3000
       });
       return;
     }
 
-    if(this.loginData.password.trim()=="" || this.loginData.password==null){
-      this.snack.open("Password is required",'',{
-        duration:3000
+    if (this.loginData.password.trim() == "" || this.loginData.password == null) {
+      this.snack.open("Password is required", '', {
+        duration: 3000
       });
       return;
     }
 
     //request server to generate token
     this.loginService.generateToken(this.loginData).subscribe(
-      (data:any) => {
-          console.log('SUCCESS:::'+data.token);
+      (data: any) => {
+        console.log('SUCCESS:::' + data.token);
 
-          //Login.....
-          this.loginService.loginUser(data.token);
-          this.loginService.getCurrentUser().subscribe(
-            (user:any) => {
-              this.loginService.setUser(user);
-              console.log("USER IN GENERATE TOKEN:::"+JSON.stringify(user));
+        //Login.....
+        this.loginService.loginUser(data.token);
+        this.loginService.getCurrentUser().subscribe(
+          (user: any) => {
+            this.loginService.setUser(user);
+            console.log("USER IN GENERATE TOKEN:::" + JSON.stringify(user));
 
-              //redirect: ADMIN -> admin-dashboard
-              //redirect: NORMAL -> normal-dashboard
+            //redirect: ADMIN -> admin-dashboard
+            //redirect: NORMAL -> normal-dashboard
 
-              if(this.loginService.getUserRole()==="ADMIN"){
-                //admin-dashboard
-                window.location.href = '/admin-dashboard';
-              }
-              else if(this.loginService.getUserRole()==="NORMAL"){
-                //user-dashboard
-                window.location.href = '/user-dashboard';
-              }
-              else{
-                this.loginService.logout();
-              }
-            },
-            (error) => {
-                this.snack.open("Invalid Details!!");
+            if (this.loginService.getUserRole() === "ADMIN") {
+              //admin-dashboard
+              this.router.navigate(['admin-dashboard']);
+              this.loginService.loginStatusSubject.next(true);
+
             }
-          );
+            else if (this.loginService.getUserRole() === "NORMAL") {
+              //user-dashboard
+              this.router.navigate(['user-dashboard']);
+              this.loginService.loginStatusSubject.next(true);
+            }
+            else {
+              this.loginService.logout();
+            }
+          },
+          (error) => {
+            this.snack.open("Invalid Details!!");
+          }
+        );
 
       },
       (error) => {
-        console.log("ERROR::"+error);
-        this.snack.open("Invalid Details!!",'',{
+        console.log("ERROR::" + error);
+        this.snack.open("Invalid Details!!", '', {
           duration: 3000
         });
       }
